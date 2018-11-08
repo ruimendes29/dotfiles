6b564961d1daefac046b39ce13bb7ca5db4793e4
Pedro Ribeiro <pedroescolaribeiro@gmail.com>
2018-11-08 17:35:34 +0000

    [vim]: Fix conflict

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-#!/bin/bash
-
-sudo apt install software-properties-common
-sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt install neovim -y
-
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-ln -sfT ~/.dotfiles/nvim/init.vim ~/.config/nvim/init.vim
-=======
 #!/usr/bin/env sh
 
 echo "Installling Neovim..."
@@ -20,4 +8,3 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 
 ln -sfT ~/.dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 
->>>>>>> 8d0471f
