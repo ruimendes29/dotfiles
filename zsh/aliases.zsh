bdd8c0bcbc597fb49a3ede4e4bb4353c7f626739
Pedro Ribeiro <pedroescolaribeiro@gmail.com>
2018-11-08 21:34:56 +0000

    [zsh]: Deleting some redundant aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,37 +1,25 @@
 # UNALIASES
 unalias please
 
-# ALIASES
-
+# "MUST HAVE" ALIASES
 alias dot="cd $HOME/.dotfiles && nvim ."
-alias spl="cd $HOME/.spells && nvim ."
 
 alias c="cd $HOME/Code && ls -l"
 alias s="cd $HOME/Snippets && ls -l"
 
-alias cv="cd $HOME/Code/curriculum-vitae/ && ls -l"
-alias ocv="cd $HOME/Code/online-curriculum/ && ls -l"
-
+# UNIVERSITY RELATED
 alias um="cd $HOME/Code/uminho/"
-alias li1="cd $HOME/Code/uminho/1st/1/Laboratórios\ de\ Informática\ I/Project"
-alias li2="cd $HOME/Code/uminho/1st/2/Laboratórios\ de\ Informática\ II/Project"
 
+# TEXT EDITING RELATED
 alias v="nvim"
 alias vim="nvim"
 alias r="ranger"
 alias p="sudo pacman"
 alias y="yaourt"
-alias ka="killall"
 alias :q="exit"
 
 alias o=open
 
 alias bsc=backup
 alias pls=please
-
-alias joão="date -d '+2 hour' '+%T' && weather Tampere Finland 0"
-alias carolina="date -d '+1 hour' '+%T' && weather Brussels Belgium 0"
-
-alias miei="slack-term --config $HOME/.slack-miei"
-alias cesium="xrandr --output HDMI1 --auto --above eDP1"
 
