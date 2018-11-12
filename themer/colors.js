88d707ccbd47254253389f8d5ae398c49103f1eb
Pedro Ribeiro <pedroescolaribeiro@gmail.com>
2018-11-12 22:07:59 +0000

    [themer]: Add themer configuraion

---
+++ themer/colors.js
@@ -0,0 +1,24 @@
+module.exports.colors = {
+
+  dark: {
+
+    accent0: '#263238',
+    accent1: '#263238',
+    accent2: '#263238',
+    accent3: '#64FCDA',
+    accent4: '#64FCDA',
+    accent5: '#64FCDA',
+    accent6: '#40C4FF',
+    accent7: '#FF5252',
+
+    shade0: '#222D31',
+    shade1: '#293845',
+    shade2: '#3F4F5B',
+    shade3: '#556572',
+    shade4: '#6A7B88',
+    shade5: '#80919F',
+    shade6: '#96A8B5',
+    shade7: '#ACBECC',
+
+    },
+};
