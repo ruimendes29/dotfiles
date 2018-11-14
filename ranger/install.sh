de8c5eb04cb7e231c7606c7cd1061be7ebffab3f
Pedro Ribeiro <pedroescolaribeiro@gmail.com>
2018-11-14 15:44:56 +0000

    [ranger]: Set default apps in ranger

--- ranger/install.sh
+++ ranger/install.sh
@@ -2,4 +2,5 @@
 
 sudo pacman -Sy ranger --noconfirm --needed --noconfirm
 ln -sfT ~/.dotfiles/ranger/rc.conf  ~/.config/ranger/rc.conf
+ln -sfT ~/.dotfiles/ranger/rifle.conf  ~/.config/ranger/rifle.conf
 
