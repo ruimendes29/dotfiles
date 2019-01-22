e6c56d1a855d7f7d1f30aead87bf29668afa11b7
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2019-01-22 23:35:08 +0000

    [haskell]: Add haskell install command

--- haskell/install.sh
+++ haskell/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 echo "Installing Haskell"
-sudo pacman -Syu ghc ghc-libs ghc-static cabal-install
+sudo pacman -S ghc ghc-libs ghc-static cabal-install
 
 ln -sfT ~/.dotfiles/haskell/ghci ~/.ghci
 
