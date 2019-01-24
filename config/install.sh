e674518c0b87e711fa2a31d5e996ed85eb9d84e0
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2019-01-24 23:40:45 +0000

    [config]: Improve readability

--- config/install.sh
+++ config/install.sh
@@ -3,5 +3,6 @@
 ln -sfT ~/.dotfiles/config/profile ~/.profile
 ln -sfT ~/.dotfiles/config/okularpartrc ~/.config/okularpartrc
 
+# Install Noto Color Emoji Font
 sudo cp ~/.dotfiles/config/50-noto-color-emoji.conf /etc/fonts/conf.d/
 
