6810b5e18a3b5c1a11e0380fc6247d7c4e93fcdd
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2019-01-24 23:39:52 +0000

    [asdf]: Improve readability

--- asdf/install.sh
+++ asdf/install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
+# Download asdf
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 
+# Connect .dotfiles asdf config with actual configuration folder
 ln -sfT ~/.dotfiles/asdf/asdfrc        ~/.asdfrc
 ln -sfT ~/.dotfiles/asdf/tool-versions ~/.tool-versions
 
