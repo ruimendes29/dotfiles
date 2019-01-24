8510e8bb0073a6047304564c1d14fe53a1fd741d
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2019-01-24 23:36:44 +0000

    [tmux]: Improve readability

--- tmux/install.sh
+++ tmux/install.sh
@@ -2,10 +2,13 @@
 
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/pwittchen/spotify-cli-linux/master/install.sh)"
 
+# Install Tmux
 echo "Installing Tmux..."
 sudo pacman -Syu tmux --needed --noconfirm
 
+# Install plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+# Link .dotfiles tmux config with actual config
 ln -sfT ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
 
