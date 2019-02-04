d6ba3ed85733cfbeb9d35d335a068a1993eba4ff
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2019-02-04 22:26:00 +0000

    [zsh]: Change theme to "pure"

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -3,7 +3,7 @@ export ZSH=~/.oh-my-zsh
 export TERM="xterm-256color"
 
 POWERLEVEL9K_MODE='awesome-fontconfig'
-ZSH_THEME="powerlevel9k/powerlevel9k"
+ZSH_THEME="refined"
 source $ZSH/oh-my-zsh.sh
 
 # My Powerlevel9k Settings
