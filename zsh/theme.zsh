bd085d4ff6a1c13a0b8541ce30d3b1d83b03508d
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2019-02-06 13:00:58 +0000

    [zsh]: Change theme to robbyrussell

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -3,7 +3,7 @@ export ZSH=~/.oh-my-zsh
 export TERM="xterm-256color"
 
 POWERLEVEL9K_MODE='awesome-fontconfig'
-ZSH_THEME="refined"
+ZSH_THEME="robbyrussell"
 source $ZSH/oh-my-zsh.sh
 
 # My Powerlevel9k Settings
