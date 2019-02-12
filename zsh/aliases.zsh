9ccb007405ee59a52cb06e9159ea73a247309841
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2019-02-12 10:04:37 +0000

    [teclado.sh]: Add alias to switch between keyboard layouts

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,11 +3,15 @@
 # "MUST HAVE" ALIASES
 alias dot="cd $HOME/.dotfiles"
 alias xmas="cd $HOME/.dotfiles/scripts && ./christmas.sh"
-
 alias c="cd $HOME/Code && ls -l"
 alias see="firefox"
+
 # UNIVERSITY RELATED
 alias um="cd $HOME/Code/uminho/"
+
+# CHANGE KEYBOARD LAYOUT
+alias ptkeyboard="setxkbmap -layout pt"
+alias gbkeyboard="setxkbmap -layout gb"
 
 # TEXT EDITING RELATED
 alias v="nvim"
